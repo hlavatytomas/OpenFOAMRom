0db0cb6c4702e039f6181e28e7c4698a90530c6b
hlavatyt <tomas7846@gmail.com>
2023-12-18 18:17:10 +0100

    More general add fncs

--- myAddFcs.py
+++ myAddFcs.py
@@ -1,5 +1,7 @@
 # -- Python script containing additional functions for OF_caseClass
 
+import numpy as np
+
 def isFloat(val):
     """function to determine if val is float"""
     try:
@@ -11,4 +13,9 @@ def isFloat(val):
 # -- function to find centeroid of the triangle
 def centroidTriangle(vertex1, vertex2, vertex3):
     centroid = (vertex1 + vertex2 + vertex3) / 3
+    return centroid
+
+# -- function to find centeroid of the general polyhedron
+def centroidGeneral(vertexes):
+    centroid = np.average(vertexes,axis=0)
     return centroid
